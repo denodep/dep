19d987e3959c15f9c8eb101f4288990228805e75
Acathur <chiahungzhang@gmail.com>
2020-06-23 00:50:36 +0800

    Update dependencies versions
    - Bump `std` version to 0.58.0
    - Bump `cmd` version to 1.1.0
    - Bump `store` version to 1.0.2

--- deps.ts
+++ deps.ts
@@ -1,19 +1,19 @@
 // std
-export * as qs from 'https://deno.land/std@0.54.0/node/querystring.ts'
-export * as path from 'https://deno.land/std@0.54.0/path/mod.ts'
-export { Sha1 } from 'https://deno.land/std@0.54.0/hash/sha1.ts'
-export { Md5 } from 'https://deno.land/std@0.54.0/hash/md5.ts'
-export { Tar } from 'https://deno.land/std@0.54.0/archive/tar.ts'
+export * as qs from 'https://deno.land/std@0.58.0/node/querystring.ts'
+export * as path from 'https://deno.land/std@0.58.0/path/mod.ts'
+export { Sha1 } from 'https://deno.land/std@0.58.0/hash/sha1.ts'
+export { Md5 } from 'https://deno.land/std@0.58.0/hash/md5.ts'
+export { Tar } from 'https://deno.land/std@0.58.0/archive/tar.ts'
 
 // third party
 import Ask from 'https://deno.land/x/ask@1.0.3/mod.ts'
 import minimatch from 'https://deno.land/x/minimatch@v3.0.4/index.js'
 import getUsername from 'https://deno.land/x/username@v1.0.0/mod.ts'
 export * as semver from 'https://deno.land/x/semver@v1.0.0/mod.ts'
-export { Command } from 'https://cdn.depjs.com/cmd@1.0.3/mod.ts'
+export { Command } from 'https://cdn.depjs.com/cmd@1.1.0/mod.ts'
 export { exec } from 'https://cdn.depjs.com/exec@1.1.0/mod.ts'
 export { getMac } from 'https://cdn.depjs.com/mac@1.1.0/mod.ts'
-export { Store } from 'https://cdn.depjs.com/store@1.0.1/mod.ts'
+export { Store } from 'https://cdn.depjs.com/store@1.0.2/mod.ts'
 export {
 	Ask,
 	getUsername,
