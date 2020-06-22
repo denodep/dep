54e71b2ba0e0e5d6bf88f088760fd915eaf4500d
Acathur <chiahungzhang@gmail.com>
2020-06-22 22:54:05 +0800

    Update cache dir

--- lib/store.ts
+++ lib/store.ts
@@ -1,8 +1,23 @@
 import { Store } from '../deps.ts'
 
+const getCachePath = () => {
+	const dirhome = Deno.dir('home')
+	const dircache = !dirhome && Deno.dir('cache')
+
+	if (dirhome) {
+		return dirhome
+	}
+	else if (dircache) {
+		return `${dircache}/deno-dep`
+	}
+	else {
+		return '.dep'
+	}
+}
+
 export const store = new Store({
 	name: '.depcache',
-	path: Deno.dir('home') || Deno.dir('cache') || '.dep'
+	path: getCachePath()
 })
 export const state: {
 	dep?: {
